df6f2ed72855d1110471c330c11bd1227550d4dd
Mohammed Obaid <mdobaid311@gmail.com>
2022-04-09 17:42:31 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,20 @@ const Header = () => {
     <div className="header-container w-3/6 m-auto">
       <div className=" header flex justify-between align-center flex-1 my-2 mx-3">
         <div className="header-logo flex align-center text-3xl font-medium">
-          Xpenser
+          Expenses
           <BsCreditCard className="flex align-center ml-2" />
         </div>
         <div className="header-button text-xl flex justify-center align-center border-[1px] border-solid border-black rounded px-2 mr-1">
           <a
             className="decoration-none"
-            href="https://github.com"
+            href="https://github.com/mdobaid311/Expense-tracker"
             target="_blank"
           >
             <i className="devicon-github-original"></i>
             Star
           </a>
         </div>
-      </div> 
+      </div>
     </div>
   );
 };
